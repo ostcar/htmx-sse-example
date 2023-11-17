0e0be68a3ccd6cf119fa3bf33ceba32cab4625c5
Oskar Hahn <mail@oshahn.de>
2023-11-17 21:40:05 +0100

    Add a test for websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.21.3
+
+require nhooyr.io/websocket v1.8.10
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
+
+	"nhooyr.io/websocket"
 )
 
 //go:embed static
@@ -22,6 +24,7 @@ func run() error {
 	http.Handle("/static/", http.FileServer(http.FS(static)))
 	http.HandleFunc("/sse", handleSSE)
 	http.HandleFunc("/poll", handlePoll)
+	http.HandleFunc("/websocket", handleWebsocket)
 	http.HandleFunc("/", handleHome)
 	log.Println("listen on :8080")
 	return http.ListenAndServe(":8080", nil)
@@ -36,15 +39,14 @@ const layout = `
 
 			<script src="/static/htmx.js"></script>
 			<script src="/static/sse.js"></script>
-
-			<!--
-			<script src="https://unpkg.com/htmx.org@1.9.6"></script>
-			<script src="https://unpkg.com/htmx.org/dist/ext/sse.js"></script>
-			-->
+			<script src="/static/websocket.js"></script>
 		</head>
 		<body>
-			<div hx-ext="sse" sse-connect="/sse" sse-swap="message"></div>
 			<div hx-get="/poll" hx-trigger="every 1s"></div>
+			<div hx-ext="sse" sse-connect="/sse" sse-swap="message"></div>
+			<div hx-ext="ws" ws-connect="/websocket">
+				<div id="websocket"></div>
+			</div>
 		</body>
 	</html>
 `
@@ -54,7 +56,7 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlePoll(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "poll %d. <strong>this is a </strong> <a href=\"https://github.com/bigskysoftware/htmx/pull/1794\">Here</a> is a fantastic pr.", rand.Intn(1000))
+	fmt.Fprintf(w, "Poll Works: %d.", rand.Intn(1000))
 }
 
 func handleSSE(w http.ResponseWriter, r *http.Request) {
@@ -66,8 +68,33 @@ func handleSSE(w http.ResponseWriter, r *http.Request) {
 	for {
 		select {
 		case <-tick.C:
-			fmt.Fprintf(w, "data: sse %d. <a href=\"https://github.com/bigskysoftware/htmx/pull/1794\">Here</a> is a fantastic pr.\n\n", rand.Intn(1000))
+			fmt.Fprintf(w, "data: SEE Works: %d.\n\n", rand.Intn(1000))
 			w.(http.Flusher).Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
+
+func handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		log.Printf("Accept websocket: %v", err)
+		return
+	}
+	defer c.CloseNow()
+
+	tick := time.NewTicker(time.Second)
+	defer tick.Stop()
+
+	for {
+		select {
+		case <-tick.C:
+			if err := c.Write(r.Context(), websocket.MessageText, []byte(fmt.Sprintf(`<div id="websocket">Websocket works: %d.</div>`, rand.Intn(1000)))); err != nil {
+				log.Printf("Send websocket message: %v", err)
+				return
+			}
+
 		case <-r.Context().Done():
 			return
 		}
